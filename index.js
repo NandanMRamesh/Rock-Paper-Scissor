114dd9b092537392d06fca02c1befa5f48bdba0d
NandanMRamesh <nandanmr2005@gmail.com>
2026-03-15 20:58:04 +0530

    add Computer and Human function

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hello world")
+console.log("welcome to the game");
+function getComputerChoice () {
+    rand = Math.random();
+    if(rand < 0.333333)
+        return "rock";
+    else if(rand > 0.333333 && rand < 0.666666)
+        return "paper";
+    else
+        return "scissor";
+}
+function getHumanChoice() {
+    let HumanInput = prompt("enter your Choice");
+    return HumanInput.toLowerCase();
+}
+
+ComputerScore = 0;
+HumanScore = 0;
+
+computerSelection = getComputerChoice();
+humanSelection = getHumanChoice();
+
+
+function playRound(computerS){}
+
