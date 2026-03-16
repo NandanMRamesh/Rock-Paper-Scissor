8f2c3c45a4a1603678a94e43694f523a9655a628
NandanMRamesh <nandanmr2005@gmail.com>
2026-03-16 21:39:38 +0530

    working code

--- index.js
+++ index.js
@@ -16,9 +16,41 @@ function getHumanChoice() {
 ComputerScore = 0;
 HumanScore = 0;
 
-computerSelection = getComputerChoice();
-humanSelection = getHumanChoice();
 
-
-function playRound(computerS){}
+function playRound(computerSelection, humanSelection){
+    console.log("human choice = "+humanSelection);
+    console.log("computer choice = "+computerSelection);
+    if(computerSelection=='rock' && humanSelection=='scissor'){
+        ComputerScore++;
+        console.log("Computer wins "+" ComputerScore="+ComputerScore + "  HumanScore="+HumanScore);
+    }
+    if(computerSelection=='rock' && humanSelection=='paper'){
+        HumanScore++;
+        console.log("Human wins "+" ComputerScore="+ComputerScore + "  HumanScore="+HumanScore);
+    }
+    if(computerSelection=='paper' && humanSelection=='rock'){
+        ComputerScore++;
+        console.log("Computer wins "+" ComputerScore="+ComputerScore + "  HumanScore="+HumanScore);
+    }
+    if(computerSelection=='paper' && humanSelection=='scissor'){
+        HumanScore++;
+        console.log("Human wins "+" ComputerScore="+ComputerScore + "  HumanScore="+HumanScore);
+    }
+    if(computerSelection=='scissor' && humanSelection=='paper'){
+        ComputerScore++;
+        console.log("Computer wins "+" ComputerScore="+ComputerScore + "  HumanScore="+HumanScore);
+    }
+    if(computerSelection=='scissor' && humanSelection=='rock'){
+        HumanScore++;
+        console.log("Human wins "+" ComputerScore="+ComputerScore + "  HumanScore="+HumanScore);
+    }
+    if(computerSelection == humanSelection){
+        console.log("round draw");
+    }
+}
+for(i = 0; i < 5; i++){
+    computerSelection = getComputerChoice();
+    humanSelection = getHumanChoice();
+    playRound (computerSelection, humanSelection)
+}
 
